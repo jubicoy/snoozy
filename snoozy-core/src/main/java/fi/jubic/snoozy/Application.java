573518491147c2d9722a230e1dc04d568ad96a6a
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2019-03-07 20:28:39 +0200

    Added WebSocketHandlers to Application
    Application::getWebSocketHandlers was added with a default
    implementation and abstract was also removed from Application::getStaticFiles.

--- snoozy-core/src/main/java/fi/jubic/snoozy/Application.java
+++ snoozy-core/src/main/java/fi/jubic/snoozy/Application.java
@@ -1,7 +1,19 @@
 package fi.jubic.snoozy;
 
+import fi.jubic.snoozy.websocket.WebSocketHandler;
+
+import java.util.Collections;
 import java.util.Set;
 
+/**
+ * Extended JAX-RS {@link javax.ws.rs.core.Application} providing static files
+ * and websockets.
+ */
 public abstract class Application extends javax.ws.rs.core.Application {
-    public abstract Set<StaticFiles> getStaticFiles();
+    public Set<StaticFiles> getStaticFiles() {
+        return Collections.emptySet();
+    }
+    public Set<WebSocketHandler> getWebSocketHandlers() {
+        return Collections.emptySet();
+    }
 }
