d9b36c18a40901886d266db748173d6bd1d4d9dd
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2019-11-02 12:51:39 +0200

    Added StaticFiles definition for default front-end

--- snoozy-core/src/main/java/fi/jubic/snoozy/Snoozy.java
+++ snoozy-core/src/main/java/fi/jubic/snoozy/Snoozy.java
@@ -1,6 +1,7 @@
 package fi.jubic.snoozy;
 
 import fi.jubic.snoozy.converters.Converters;
+import fi.jubic.snoozy.filters.UrlRewrite;
 import fi.jubic.snoozy.mappers.Mappers;
 
 import java.util.Set;
@@ -13,5 +14,20 @@ public final class Snoozy {
                 Converters.builtins().stream(),
                 Mappers.builtins().stream()
         ).collect(Collectors.toSet());
+    }
+
+    public static StaticFiles defaultFrontend() {
+        UrlRewrite rewrite = UrlRewrite.of(
+                "^\\/(?!(((api|assets|images|fonts|css).*)|.*\\.(html|js)$)).*$",
+                "/index.html"
+        );
+
+        return StaticFiles.builder()
+                .setPrefix("static")
+                .setClassLoader(Application.class.getClassLoader())
+                .setMethodAccess(MethodAccess.anonymous())
+                .setRewrite(rewrite)
+                .build();
+
     }
 }
