f399071083d9459d9231431d484a8b905b3d51a4
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2020-02-07 11:39:34 +0200

    Fix NPEs with missing User-Agent header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres poorly to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fix
+- Fix NPEs with missing `User-Agent` header.
 
 ## [0.6.0] - 2020-02-03
 ### Added
--- snoozy-core/src/main/java/fi/jubic/snoozy/logging/DefaultLoggingFilter.java
+++ snoozy-core/src/main/java/fi/jubic/snoozy/logging/DefaultLoggingFilter.java
@@ -8,6 +8,7 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
+import java.util.Optional;
 
 public class DefaultLoggingFilter implements LoggingFilter {
     private static final Logger logger = LoggerFactory.getLogger(
@@ -41,20 +42,23 @@ public class DefaultLoggingFilter implements LoggingFilter {
         long duration = System.currentTimeMillis() - startTime;
 
         logger.info(
-                "{} {}{} {} {} {} {}ms",
-                containerRequestContext.getMethod()
-                        .replaceAll("[\r\n]",""),
-                request.getContextPath()
-                        .replaceAll("[\r\n]",""),
-                containerRequestContext.getUriInfo()
-                        .getPath()
-                        .replaceAll("[\r\n]",""),
-                containerResponseContext.getStatus(),
-                request.getRemoteHost()
-                        .replaceAll("[\r\n]",""),
-                containerRequestContext.getHeaderString(HttpHeaders.USER_AGENT)
-                        .replaceAll("[\r\n]",""),
-                duration
+                String.format(
+                        "%s %s%s %d %s %s %dms",
+                        containerRequestContext.getMethod(),
+                        request.getContextPath(),
+                        containerRequestContext.getUriInfo()
+                                .getPath(),
+                        containerResponseContext.getStatus(),
+                        request.getRemoteHost(),
+                        Optional
+                                .ofNullable(
+                                        containerRequestContext.getHeaderString(
+                                                HttpHeaders.USER_AGENT
+                                        )
+                                )
+                                .orElse("-"),
+                        duration
+                ).replaceAll("[\r\n]","")
         );
     }
 }
