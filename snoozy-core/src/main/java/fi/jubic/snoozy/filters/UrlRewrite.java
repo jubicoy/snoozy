39367207169eba10a4a5ab472d1c8cd4031dabf6
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2020-05-10 15:39:16 +0300

    Remove deprecated getters and a builder from UrlRewrite

--- snoozy-core/src/main/java/fi/jubic/snoozy/filters/UrlRewrite.java
+++ snoozy-core/src/main/java/fi/jubic/snoozy/filters/UrlRewrite.java
@@ -13,58 +13,11 @@ public class UrlRewrite {
         return new UrlRewrite(from, to);
     }
 
-    @Deprecated
-    public String from() {
-        return getFrom();
-    }
-
     public String getFrom() {
         return from;
     }
 
-    @Deprecated
-    public String to() {
-        return getTo();
-    }
-
     public String getTo() {
         return to;
-    }
-
-    @Deprecated
-    public Builder toBuilder() {
-        return new Builder(from, to);
-    }
-
-    @Deprecated
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    @Deprecated
-    public static class Builder {
-        private final String from;
-        private final String to;
-
-        private Builder() {
-            this(null, null);
-        }
-
-        private Builder(String from, String to) {
-            this.from = from;
-            this.to = to;
-        }
-
-        public Builder setFrom(String from) {
-            return new Builder(from, to);
-        }
-
-        public Builder setTo(String to) {
-            return new Builder(from, to);
-        }
-
-        public UrlRewrite build() {
-            return new UrlRewrite(from, to);
-        }
     }
 }
